63777280abe914eed51012917b8ccaceb3decb61
Psychrode <97118606+Psychrode@users.noreply.github.com>
2022-09-19 04:03:40 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -34,12 +34,6 @@ public class Book extends Media{
 	
 	@Override
 	public int compareTo(Media o) {
-		/*1. Books come before Movies, Movies come before Series, Series come before Music
-		2. Books are sorted first by author, then by title
-		3. Movies are sorted first by title, then by year
-		4. Series are sorted just by title
-		5. Music is sorted first by artist, then by year, then by title
-		*/
 
 		if(! (o instanceof Book)) {
 			return -1;
