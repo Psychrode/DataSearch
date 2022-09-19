2142cb013fd2257210c7a50723497063a8820a13
Psychrode <97118606+Psychrode@users.noreply.github.com>
2022-09-19 04:10:31 -0400

    Update SearchFilter.java

--- SearchFilter.java
+++ SearchFilter.java
@@ -7,11 +7,6 @@ public class SearchFilter implements MediaFilter{
 	}
 	
 	public boolean matches(Label<String, Media> a) {
-		/*
-		 * returns true if the label contains the searchTerm, or if the
-		 * searchTerm is in the string returned by the toString method of the media item. You may find
-		 * the string method indexOf useful for this.
-		 */
 		if(a.toString().indexOf(searchTerm) == -1) {
 			return false;
 		}
